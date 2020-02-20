9581098774ae08050286c98f9a17b4cf16ec760a
Kristin Barr <kristinbarr@gmail.com>
2020-02-20 15:04:48 -0500

    recipe batches complete

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,26 +4,23 @@ import math
 
 def recipe_batches(recipe, ingredients):
 # var for batches dict
-  batches = {}
+  batches = []
 
 # iterate over recipe 
   for key in recipe.keys():
-    if key in ingredients and ingredients[key] > recipe[key]:
-      print('batch is possible')
-      batches[key] = 
-    else: 
-      print('batch is not possible')
+    # if key exist and enough igredients for 1 batch,
+    if key in ingredients and ingredients[key] >= recipe[key]:
+      # save batch count to ingredient batch list
+      batches.append(ingredients[key] // recipe[key])
+    else:
       return 0
+  # find smallest batch number
+  return min(batches)
 
-  # if key doesnt exist, return 0
-  # elif recipe key >= ingredient, return 0
 
-recipe_batches({ 'milk': 100, 'butter': 50, 'cheese': 10 }, { 'milk': 198, 'butter': 52, 'cheese': 10 })
-# recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 })
-
-# if __name__ == '__main__':
-#   # Change the entries of these dictionaries to test
-#   # your implementation with different inputs
-#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+if __name__ == '__main__':
+  # Change the entries of these dictionaries to test
+  # your implementation with different inputs
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
